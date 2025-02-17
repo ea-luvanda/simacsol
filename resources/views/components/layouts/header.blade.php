5b6203887e309a956de3ed0b8ddfa0c3cba11cae
ea-luvanda <e.a.luvanda@gmail.com>
2025-02-18 02:40:25 +0300

    header margin button

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <!-- Navigation -->
-<nav class="sticky top-0 z-50 bg-white shadow-none">
+<nav class="sticky top-0 z-50 bg-white shadow-none mb-8">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
